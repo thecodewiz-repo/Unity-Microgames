48babbb75657ae5de9240ea7c9da67c36eea22d3
unknown <micposso@gmail.com>
2021-05-28 06:19:16 -0400

    add console log script

---
+++ scripts/LegoGameStart.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.LEGO.Minifig {
+  public class LegoGameStart : MonoBehaviour {
+      void Start() {
+        Debug.Log("Hello: " + gameObject.name);
+      }
+    }
+}
