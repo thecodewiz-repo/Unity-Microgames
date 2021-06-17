3a1c7bbe4d39f778d4a8f896aff12c51ade33e98
michael posso <micposso@gmail.com>
2021-06-17 10:55:30 -0400

    Update LegoGameStart.cs

--- scripts/LegoGameStart.cs
+++ scripts/LegoGameStart.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Unity.LEGO.Minifig {
   public class LegoGameStart : MonoBehaviour {
-      void Start() {
+      void Awake() {
         Debug.Log("Hello: " + gameObject.name);
       }
     }
